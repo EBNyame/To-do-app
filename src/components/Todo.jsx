da0859d37fe179b530d1d90d694b6af9dcafea18
ebnyame <ebnyame001@st.ug.edu.gh>
2024-06-24 11:01:11 -0700

    added the funtionality that enables the user to add something to Todo lists

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    console.log(inputText);
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h[550px] h-2/3	 rounded-xl">
       {/* --------------Title-------------- */}
@@ -15,19 +35,24 @@ const Todo = () => {
       {/* --------------Input box-------------- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           ADD +
         </button>
       </div>
 
       {/* --------------Todo List-------------- */}
       <div>
-        <TodoItems text="learn Coding" />
-        <TodoItems text="Learn Coding by yourself, with yourself" />
+        {todoList.map((item, index) => {
+          return <TodoItems key={index} text={item.text} />;
+        })}
       </div>
     </div>
   );
